081b09ea6bbc54bad3f81f28dc3af52fd57672d4
supun19 <supun12.@cse.mrt.ac.lk>
2017-07-30 09:13:41 +0530

    phone verification ui

--- app/src/main/java/com/example/supun/food/MainActivity.java
+++ app/src/main/java/com/example/supun/food/MainActivity.java
@@ -3,11 +3,26 @@ package com.example.supun.food;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+    private void updateUI(FirebaseUser user) {
+
     }
 }
